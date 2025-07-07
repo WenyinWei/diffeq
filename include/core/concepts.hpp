770a2ffdff404882a41a4f7f9eb27b3f9435125f
Wenyin Wei <wenyin.wei.ww@gmail.com>
2025-07-07 20:25:33 +0800

    Refactor system_state concept in core concepts header
    - Removed unnecessary iterator requirements for the system_state concept.
    - Added requirement for direct access to elements using the subscript operator.
    - Improved clarity and usability of the concept definition.

--- include/core/concepts.hpp
+++ include/core/concepts.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <concepts>
 #include <type_traits>
-#include <iterator>
 #include <string>
 
 // Time type concept - basic arithmetic types that can represent time
@@ -14,9 +13,8 @@ concept system_state = requires(T state) {
     typename T::value_type;
     requires std::is_arithmetic_v<typename T::value_type>;
     requires !std::same_as<T, std::string>; // Exclude string types
-    requires requires {
-        { state.size() } -> std::convertible_to<std::size_t>;
-        { state.begin() } -> std::random_access_iterator;
-        { state.end() } -> std::random_access_iterator;
-    };
+    { state.size() } -> std::convertible_to<std::size_t>;
+    { state.begin() };
+    { state.end() };
+    { state[0] } -> std::convertible_to<typename T::value_type>;
 };
